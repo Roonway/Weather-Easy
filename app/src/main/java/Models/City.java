291a6a636e2eb2edeeb1f608820923afbb0919c3
Roney <roneyjoab@gmail.com>
2019-12-01 16:12:47 -0300

    models

--- app/src/main/java/Models/City.java
+++ app/src/main/java/Models/City.java
@@ -1,9 +1,11 @@
-package com.example.weathereasy;
+package Models;
 
 public class City {
     private String name;
     private String countryCode;
     private short log;
     private short lat;
+
+
 
 }
